e598f17a19dcd8b003912b41a8a0861adb01d326
luwilliam2017 <103793563@qq.com>
2017-11-11 19:00:57 +0800

    first commit

---
+++ src/main/java/page/Application.java
@@ -0,0 +1,29 @@
+package page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by w on 2017/11/11.
+ */
+@Configuration
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/test/ApplicationTest.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by w on 2017/11/11.
+ */
+public class ApplicationTest {
+}
